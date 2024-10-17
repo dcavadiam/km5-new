fc262724b31c1f482616b3e5cc937451c780cc3c
dcavadia <dcavadia@uninorte.edu.co>
2024-10-17 11:31:40 -0500

    nuevas páginas

---
+++ global/script.js
@@ -0,0 +1,8 @@
+// Sidebar icon
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
